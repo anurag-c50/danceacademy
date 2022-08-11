6520fe33c728c3cf13eb058ae887b64021b70d87
ajjha5244@gmail.com <105697066+anurag-c50@users.noreply.github.com>
2022-08-11 13:52:38 +0530

    ghj

--- static/ada.js
+++ static/ada.js
@@ -7,7 +7,7 @@ $('.Mysection').slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 400,
+      breakpoint: 700,
       settings: {
         autoplay: true,
         autoplaySpeed: 2000,
@@ -30,4 +30,26 @@ $('.Mysection').slick({
     }
 
  }]
+  });
+  const mainMenu = document.querySelector('.mainMenu');
+  const closeMenu = document.querySelector('.closeMenu');
+  const openMenu = document.querySelector('.openMenu');
+  const menu_items = document.querySelectorAll('nav .mainMenu li a');
+  
+  openMenu.addEventListener('click',show);
+  closeMenu.addEventListener('click',close);
+  
+  // close menu when you click on a menu item 
+  menu_items.forEach(item =>{
+      item.addEventListener('click',function(){
+          close();
+      })
   })
+  
+  function show(){
+      mainMenu.style.display = 'flex';
+      mainMenu.style.top = '0';
+  }
+  function close(){
+      mainMenu.style.top = '-100%';
+  }
